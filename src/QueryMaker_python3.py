7b8cc72e3bdac5e61f8a7ee4cc58c40898ce47f6
henryhansollee <henryhansollee@gmail.com>
2022-02-15 00:22:29 +0900

    Update QueryMaker_python3

--- src/QueryMaker_python3.py
+++ src/QueryMaker_python3.py
@@ -2,16 +2,21 @@
 path = './input.txt'
 f = open(path, 'r')
 lines = f.readlines()
-
 row = len(lines)
 
 # INPUTS
 tableName = 'alphabet'
-fields = []
-values = []
+fields = ['(', '`field1`, ', '`field2`, ', '`field3`, ', '`field4`, ', '`field5`, ', '`field6`, ', '`field7` ', ')']
+col = len(fields) - 2
+values = [[''] * col for _ in range(row)]
+for i in range(row):
+    for j in range(col):
+        values[i][j] = lines[i].split()[j]
 
 # QUERY - INSERT
-queryFrameInsert = ['INSERT INTO', tableName, fields, 'VALUES', values, ';']
+for i in range(row):
+    queryFrameInsert = ['INSERT INTO', tableName, ''.join(fields), 'VALUES', values[i], ';']
+
 
 # RESULTS
 for i in range(row):
