d0285b0a1b91eedc4819bc3a064737932baacb55
henryhansollee <henryhansollee@gmail.com>
2022-02-21 22:20:15 +0900

    Update QueryMaker_python3

--- src/QueryMaker_python3.py
+++ src/QueryMaker_python3.py
@@ -8,16 +8,20 @@ row = len(lines)
 tableName = 'alphabet'
 fields = ['(', '`field1`, ', '`field2`, ', '`field3`, ', '`field4`, ', '`field5`, ', '`field6`, ', '`field7` ', ')']
 col = len(fields) - 2
-values = [[''] * col for _ in range(row)]
+values = [[''] * (col + 2) for _ in range(row)]
 for i in range(row):
-    for j in range(col):
-        values[i][j] = lines[i].split()[j]
+    values[i][0] = '('
+    values[i][col+1] = ')'
+    for j in range(1, col+1):
+        if j == col:
+            values[i][j] = lines[i].split()[j - 1]
+        else:
+            values[i][j] = lines[i].split()[j-1] + ', '
 
 # QUERY - INSERT
 for i in range(row):
-    queryFrameInsert = ['INSERT INTO', tableName, ''.join(fields), 'VALUES', values[i], ';']
-
+    queryFrameInsert = ['INSERT INTO ', tableName + ' ', ''.join(fields), ' VALUES ', ''.join(values[i]), ';']
 
 # RESULTS
 for i in range(row):
-    print(queryFrameInsert)
+    print(''.join(queryFrameInsert))
